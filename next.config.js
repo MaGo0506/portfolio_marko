3fe520b1d5ca58313dc9675afa7de667bbb0867e
Marko Goronjic <goronjicmarko24@gmail.com>
2024-01-14 15:26:38 +0100

    - sharp excluded fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.module.rules.push({
+          test: /sharp/,
+          use: 'null-loader',
+        });
+      }
+  
+      return config;
+    },
+};
