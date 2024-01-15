c24460c940c657f7a8f0fa90227e41ad1a91e902
Marko Goronjic <goronjicmarko24@gmail.com>
2024-01-15 17:38:32 +0100

    Project cards fixed

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -11,14 +11,20 @@ const ProjectCard: React.FC<{ project: Project; isEven: boolean }> = ({ project,
     <div className={`flex my-20 gap-4 ${isEven ? 'flex-row-reverse' : ''}`}>
       <div className="flex-shrink-0 relative">
         <Image className="rounded-md shadow-md" src={project.thumbnail} alt={project.title} width={500} height={400} />
-        <a href={project.link} target="_blank" className="absolute top-0 left-0 h-full w-full z-10 bg-sky-400 opacity-70 hover:opacity-0 transition-opacity duration-200 rounded-md"/>
+        <a 
+          href={project.link} 
+          target="_blank" 
+          className="absolute top-0 left-0 h-full w-full z-10 bg-sky-400 opacity-40 hover:opacity-0 transition-opacity duration-200 rounded-md"
+        />
       </div>
 
       <div className={`flex flex-col justify-between ${!isEven ? 'text-right items-end' : 'text-left'}`}>
         <div>
-        <p className="text-sky-400 text-sm">Featured Project</p>
-        <h2 className="text-2xl font-semibold mb-2">{project.title}</h2>
-        <p className="mb-4 text-gray-500 rounded-sm dark:text-gray-300 p-3 bg-gray-300 dark:bg-gray-800 shadow-md">{project.description}</p>
+          <p className="text-sky-400 text-sm">Featured Project</p>
+          <h2 className="text-2xl font-semibold mb-2">{project.title}</h2>
+          <p className="mb-4 text-gray-500 rounded-sm dark:text-gray-300 p-3 bg-gray-300 dark:bg-gray-800 shadow-md">
+          {project.description}
+          </p>
         
           <ul className={`list-none flex gap-3 text-sky-400 ${!isEven ? 'justify-end' : 'justify-start'}`}>
             {project.technologies.map((tech, index) => (
