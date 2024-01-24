d95c6a35eddcd21acadfe9ba7d59ec51f7d99ff7
Marko Goronjic <goronjicmarko24@gmail.com>
2024-01-24 19:52:30 +0100

    - refactor

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,11 +23,11 @@ const Hero = () => {
   return (
     <div ref={heroRef} className="relative h-screen flex items-center container mx-auto fade">
       <div className="relative text-left bg-opacity-50 py-8 rounded-md backdrop-filter backdrop-blur-md">
-      <p className="font-normal mb-5 text-base lg:text-lg">Hi, I&#39;m </p>
-        <h1 className="font-bold mb-4 max-w-7xl flex flex-col">
-          <span className="underline text-sky-400 text-4xl lg:text-6xl mb-3">Marko Goronjic, a Web Developer.</span>
-          <span className="text-4xl lg:text-6xl text-gray-500">I build apps for the web.</span>
-        </h1>
+        <p className="font-normal mb-5 text-base lg:text-lg">Hi, I&#39;m </p>
+          <h1 className="font-bold mb-4 max-w-7xl flex flex-col">
+            <span className="underline text-sky-400 text-4xl lg:text-6xl mb-3">Marko Goronjic, a Web Developer.</span>
+            <span className="text-4xl lg:text-6xl text-gray-500">I build apps for the web.</span>
+          </h1>
         <p className="mb-5 text-gray-500 dark:text-gray-400 text-sm lg:text-base">I’m a web developer specializing in building (and occasionally designing) <br/> exceptional digital experiences.</p>
         <Button text="Get in contact" onClick={scrollToContact} />
       </div>
