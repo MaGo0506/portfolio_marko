bc079864e30d1a54609ee5f3eb1296761e5f1c4e
Marko Goronjic <goronjicmarko24@gmail.com>
2024-02-07 10:06:08 +0100

    - refactor

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './Footer.module.css';
-import { useTheme } from 'next-themes'
+import { useTheme } from 'next-themes';
 
 const Footer = () => {
   const { theme, setTheme } = useTheme();
