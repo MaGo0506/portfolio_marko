a7da4a16502b309c4e7fbcaeae80f509d68db560
Marko Goronjic <goronjicmarko24@gmail.com>
2024-02-18 10:34:18 +0100

    - projects section font family changed

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -6,6 +6,7 @@ import { Project } from './types';
 import { useTheme } from 'next-themes';
 import styles from './Projects.module.css';
 import { setupIntersectionObserver } from '@/app/utils/intersectionObserver';
+import fonts from '@/app/fonts';
 
 
 const ProjectCard: React.FC<{ project: Project; isEven: boolean }> = ({ project, isEven }) => {
@@ -31,13 +32,13 @@ const ProjectCard: React.FC<{ project: Project; isEven: boolean }> = ({ project,
 
       <div className={`flex flex-col justify-between ${!isEven ? 'text-right items-end' : 'text-left'}`}>
         <div>
-          <p className="text-sky-400 text-xs lg:text-sm">Featured Project</p>
-          <h2 className="text-lg lg:text-2xl font-semibold mb-2">{project.title}</h2>
-          <p className="mb-4 text-gray-500 dark:text-gray-300 rounded-sm text-sm lg:text-base p-3 bg-gray-300 dark:bg-gray-800 shadow-md">
+          <p className={`text-sky-400 text-xs lg:text-sm ${fonts.inter.className}`}>Featured Project</p>
+          <h2 className={`text-lg lg:text-2xl font-semibold mb-2 ${fonts.poppins.className}`}>{project.title}</h2>
+          <p className={`mb-4 text-gray-500 dark:text-gray-300 rounded-sm text-sm lg:text-base p-3 bg-gray-300 dark:bg-gray-800 shadow-md ${fonts.inter.className}`}>
           {project.description}
           </p>
         
-          <ul className={`list-none text-sm lg:text-base flex gap-1 md:gap-3 text-sky-400 ${!isEven ? 'justify-end' : 'justify-start'}`}>
+          <ul className={`list-none text-sm lg:text-base flex gap-1 md:gap-3 text-sky-400 ${!isEven ? 'justify-end' : 'justify-start'} ${fonts.inter.className}`}>
             {project.technologies.map((tech, index) => (
               <li key={index}>{tech}</li>
             ))}
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,11 +1,12 @@
 import { projects } from './data';
 import ProjectCard from '@/components/Projects/ProjectCard';
+import fonts from '@/app/fonts';
 
 const Projects = () => {
     return ( 
         <div id="projects" className="lg:mb-72 mb-32 pt-20 container mx-auto">
             <div className="flex items-center mb-8">
-                <h1 className="font-semibold text-2xl lg:text-4xl">Some of the things I&#39;ve worked on</h1>
+                <h1 className={`font-semibold text-2xl lg:text-4xl ${fonts.poppins.className}`}>Some of the things I&#39;ve worked on</h1>
                 <div className="line"/>
             </div>
             <div>
