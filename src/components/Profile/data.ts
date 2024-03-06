fc3b2c8e56b25c4a9697f5e8dba27f2d56b1e6ec
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-06 16:04:19 +0100

    - technologies added

--- src/components/Profile/data.ts
+++ src/components/Profile/data.ts
@@ -1,7 +1,7 @@
 import { TechnologyGroup } from "./types";
 
 export const technologies: TechnologyGroup[] = [
-    { technologies: ['Javascript', 'React', 'Vue (2, 3)', 'Next.js', 'Nuxt'] },
-    { technologies: ['Node.js', 'Typescript', 'Tailwind', 'Boostrap', 'Sass'] },
-    { technologies: ['Webflow', 'Redux', 'Vuex', 'HTML', 'CSS'] },
+    { technologies: ['Javascript', 'React', 'Vue (2, 3)', 'Next.js', 'Nuxt', 'Typescript'] },
+    { technologies: ['Node.js', 'Express', 'MongoDB', 'Tailwind', 'Boostrap', 'Figma'] },
+    { technologies: ['Webflow', 'Redux', 'Vuex', 'HTML', 'CSS', 'Sass'] },
 ];
