db5f28264aa8cf17dfbad9146a14568fca29a5b0
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-28 23:03:01 +0100

    - refactor

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,15 +2,15 @@ import ContactForm from "@/components/Contact/ContactForm";
 import fonts from "@/app/fonts";
 
 const Contact = () => {
-    return ( 
+    return (
         <div id="contact" className="lg:mb-72 mb-32 pt-20 container mx-auto">
-             <div className="flex items-center gap-5 mb-8">
+            <div className="flex items-center gap-5 mb-8">
                 <h1 className={`font-semibold text-nowrap text-2xl lg:text-4xl ${fonts.poppins.className}`}>Get In Touch</h1>
-                <div className="line"/>
+                <div className="line" />
             </div>
             <ContactForm />
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
